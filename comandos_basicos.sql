6cd640091ca07e51fa0ed1a471bdc0fa3d5a78ac
Luciane Souza <54473729+souzalu@users.noreply.github.com>
2023-11-28 21:59:15 -0300

    Update comandos_basicos.sql

--- comandos_basicos.sql
+++ comandos_basicos.sql
@@ -1,6 +1,6 @@
--- EXERCÍCIOS ######################################################################
+-- Comandos Básicos
 
--- (Exercício 1) Calcule quantos salários mínimos ganha cada cliente da tabela 
+-- Calcule quantos salários mínimos ganha cada cliente da tabela 
 -- sales.customers. Selecione as colunas de: email, income e a coluna calculada "salários mínimos"
 -- Considere o salário mínimo igual à R$1200
 select 
@@ -9,7 +9,7 @@ select
 	(income)/1200 as "salários mínimos"
 from sales.customers 
 
--- (Exercício 2) Na query anterior acrescente uma coluna informando TRUE se o cliente
+-- Na query anterior acrescente uma coluna informando TRUE se o cliente
 -- ganha acima de 5 salários mínimos e FALSE se ganha 4 salários ou menos.
 -- Chame a nova coluna de "acima de 4 salários"
 select
@@ -19,7 +19,7 @@ select
 	((income) / 1200) > 4 as "acima de 4 salários"
 from sales.customers
 
--- (Exercício 3) Na query anterior filtre apenas os clientes que ganham entre
+--Na query anterior filtre apenas os clientes que ganham entre
 -- 4 e 5 salários mínimos. Utilize o comando BETWEEN
 select
 	email,
@@ -29,14 +29,14 @@ select
 from sales.customers
 where ((income) / 1200) between 4 and 5
 
--- (Exercício 4) Selecine o email, cidade e estado dos clientes que moram no estado de 
+--Selecine o email, cidade e estado dos clientes que moram no estado de 
 -- Minas Gerais e Mato Grosso. 
 select email, city, state
 from sales.customers
 where state in ('MT', 'MG')
 
 
--- (Exercício 5) Selecine o email, cidade e estado dos clientes que não 
+--Selecine o email, cidade e estado dos clientes que não 
 -- moram no estado de São Paulo.
 select email, city, state
 from sales.customers
